2f5d1ad6a386ee4dc51001e0f452b7887b558519
ThomasKyle <thomas19992010@hotmail.com>
2021-01-24 20:12:15 -0500

    made search bar

---
+++ assets/js/script.js
@@ -0,0 +1,95 @@
+var lat;
+var lon;
+
+var happySongs = 'playlist/1479458365';
+var sadSongs = 'playlist/1911334042';
+var fearSongs = 'playlist/8646459442';
+var surpriseSongs = 'playlist/8651277862';
+
+var happyBtn = document.getElementById('happy-play');
+var sadBtn = document.getElementById('sad-play');
+var fearBtn = document.getElementById('fear-play')
+var surpriseBtn = document.getElementById('surprise-play');
+
+function getMyLocation() {
+    if(navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(function(position){
+        console.log(position);
+        lat = position.coords.latitude;
+        lon = position.coords.longitude;
+        fetch(
+            'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly,alerts&appid=f7e68c78a6c0589ffc5c75fdd1fe6b01')
+        .then(function (response) {
+            response.json()
+            .then(function (data) {
+                console.log("Weather data: ", data);
+            })
+        })
+        console.log("Coordinates: ", lat, lon);
+    });
+    else {
+        console.log("Geolocation not available on device");
+    }
+}
+getMyLocation();
+//get songs depending on button clicked
+
+
+    fearBtn.addEventListener('click', () => {
+        fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/' + fearSongs)
+        .then(function (response) {
+            response.json()
+            .then(function (data){
+                console.log("DEEZER DATA: ", data);
+            })
+        })
+    });
+
+    happyBtn.addEventListener('click', () => {
+        fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/' + happySongs)
+        .then(function (response) {
+            response.json()
+            .then(function (data){
+                console.log("DEEZER DATA: ", data);
+            })
+        })
+    });
+
+    sadBtn.addEventListener('click', () => {
+        fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/' + sadSongs)
+        .then(function (response) {
+            response.json()
+            .then(function (data){
+                console.log("DEEZER DATA: ", data);
+            })
+        })
+    });
+
+    surpriseBtn.addEventListener('click', () => {
+        fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/' + surpriseSongs)
+        .then(function (response) {
+            response.json()
+            .then(function (data){
+                console.log("DEEZER DATA: ", data);
+            })
+        })
+    });
+
+
+//search bar
+function searchFunction() {
+    var searchInput = document.querySelector('search-input');
+    console.log(data)
+
+    fetch(
+ 'https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=' + searchInput
+    )
+    .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+        console.log(response.data[0]);
+      });
+
+      window.open(searchInput, '_blank')
+}
